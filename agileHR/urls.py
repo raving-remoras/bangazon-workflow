49b106464f8055084973382e1992b6eb68bcdc43
Sebastian Civarolo <scivarolo@gmail.com>
2019-01-28 11:37:34 -0600

    Display list of computers and view details for each one.

--- agileHR/urls.py
+++ agileHR/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("employees/", views.employee, name="employee"),
     path("departments/", views.department, name="department"),
     path("trainings/", views.training, name="training"),
-    path("computers/", views.computer, name="computer")
+    path("computers/", views.computer, name="computer"),
+    path("computers/<int:computer_id>", views.computer_detail, name="computer_detail")
 ]
--- agileHR/views.py
+++ agileHR/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Employee, Department, Training, Computer
+
+from .models import *
 
 def index(request):
     context = {}
@@ -19,6 +20,38 @@ def training(request):
     context = {}
     return render(request, 'agileHR/training.html', context)
 
+
 def computer(request):
-    context = {}
+    """Displays the list of computers currently owned by the company with links to details for each one.
+
+    Author: Sebastian Civarolo
+
+    Returns:
+        render -- loads the computer.html template.
+    """
+
+    computers = Computer.objects.all()
+    context = {
+        "computers": computers
+    }
     return render(request, 'agileHR/computer.html', context)
+
+
+def computer_detail(request, computer_id):
+    """Displays the details about a single computer owned by the company.
+
+    Arguments:
+        computer_id {int} -- The pk of the computer being requested.
+
+    Returns:
+        render -- Returns the computer_detail template
+    """
+    computer = Computer.objects.get(pk=computer_id)
+    computer_employee = EmployeeComputer.objects.filter(computer_id=computer_id)
+
+    context = {
+        "computer": computer,
+        "computer_employee": computer_employee
+    }
+
+    return render(request, "agileHR/computer_detail.html", context)
