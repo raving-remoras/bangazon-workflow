3b612346542a8f2a06301b97f44b62e562604357
Brendan McCray <rbmccr@gmail.com>
2019-01-28 09:56:36 -0600

    began handling departments

--- agileHR/views.py
+++ agileHR/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Department
 
 def index(request):
     context = {}
-    return render(request, 'agileHR/index.html', context)
+    return render(request, "agileHR/index.html", context)
 
 def employee(request):
     context = {}
-    return render(request, 'agileHR/employee.html', context)
+    return render(request, "agileHR/employee.html", context)
 
 def department(request):
-    context = {}
-    return render(request, 'agileHR/department.html', context)
+    departments = get_object_or_404(Department)
+    context = {
+        "departments": departments
+    }
+    return render(request, "agileHR/department.html", context)
 
 def training(request):
     context = {}
-    return render(request, 'agileHR/training.html', context)
+    return render(request, "agileHR/training.html", context)
 
 def computer(request):
     context = {}
-    return render(request, 'agileHR/computer.html', context)
+    return render(request, "agileHR/computer.html", context)
